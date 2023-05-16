f098efb1133b83fc4060c6ca821de7acf7484204
Fadi Hania <fhania@gmail.com>
2023-05-16 22:57:23 +0300

    Create new controller inside Controllers folder

---
+++ AspNetCoreMvc/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿namespace AspNetCoreMvc.Controllers;
+
+public class HomeController
+{
+    // "/"
+    // "/Home"
+    // "/Home/Index"
+    public string Index()
+    {
+        return "Home page";
+    }
+}
