b67639b4924d074778d73cccb4fd4d48ae7cfd5a
Fadi Hania <fhania@gmail.com>
2023-05-29 19:59:08 +0300

    Use views by specifying full path including extension

--- AspNetCoreMvc/Controllers/HomeController.cs
+++ AspNetCoreMvc/Controllers/HomeController.cs
@@ -11,7 +11,8 @@ public class HomeController : Controller
     {
         //return Json("Home page");
         //return Content("Home page");
-        return Content("<h1>Home page</h1>", "text/html");
+        //return Content("<h1>Home page</h1>", "text/html");
+        return View("First.cshtml");
     }
 
     // "/Home/Bio"
